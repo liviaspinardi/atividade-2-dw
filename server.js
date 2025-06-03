6952cf9a9c5134e39b61dd2a43d67a1f3febb53d
liviaspinardi <spinardi.liv@gmail.com>
2025-06-02 23:10:52 -0300

    Correção 2

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
 
 app.use(express.json());
 
